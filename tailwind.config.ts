cc2dbc1455f33dc958251c3acd54b8e0ed44b96f
Bayathy <misosoup0321@icloud.com>
2023-07-05 11:42:13 +0900

    feat:theme-colorの定義

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,11 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     colors: {
-      background: "#F5EFE7",
+      beige: "#F5EFE7",
+      purple: "#9376E0",
+      pink: "#E893CF",
+      orange: "#F2BE22",
+      blue: "#2192FF",
     },
   },
   plugins: [animate],
