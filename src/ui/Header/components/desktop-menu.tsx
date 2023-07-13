887f41743fd12a3bb3c479e3d5fd4ad22b1dc9af
Bayathy <misosoup0321@icloud.com>
2023-07-14 01:01:31 +0900

    feat: desktopMenuをキーボード操作可能に

--- src/ui/Header/components/desktop-menu.tsx
+++ src/ui/Header/components/desktop-menu.tsx
@@ -3,9 +3,10 @@ import type { MenuProps } from "./types";
 export function DesktopMenu({ menuList }: MenuProps) {
   return (
     <ul className={"hidden gap-4 md:flex"}>
-      {menuList.map((index) => (
+      {menuList.map((index, key) => (
         <li key={index.href}>
           <a
+            tabIndex={key + 1}
             href={index.href}
             className={"underline duration-300 hover:text-purple"}
           >
