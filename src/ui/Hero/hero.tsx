50a49a28a9b2ff181f1bb3dfa5545c2da1fc407c
Bayathy <misosoup0321@icloud.com>
2023-10-11 23:29:13 +0900

    feat: リリース予定日表記の追加

--- src/ui/Hero/hero.tsx
+++ src/ui/Hero/hero.tsx
@@ -17,16 +17,19 @@ export function Hero() {
         >
           <h1 className={"text-2xl"}>イベントにアソビ心を</h1>
           {searchParams.get("event_id") ? (
-            <a
-              className={
-                "w-max rounded-xl bg-blue px-4 py-2 text-white shadow-lg hover:bg-blue/80"
-              }
-              href={`repaint://app.repaint.asgs.dev/?event_id=${searchParams.get(
-                "event_id",
-              )}`}
-            >
-              イベントに参加する
-            </a>
+            <div className="flex flex-col items-center gap-2">
+              <p className={"text-xl"}>10月12日リリース予定</p>
+              <a
+                className={
+                  "w-max rounded-xl bg-blue px-4 py-2 text-white shadow-lg hover:bg-blue/80"
+                }
+                href={`repaint://app.repaint.asgs.dev/?event_id=${searchParams.get(
+                  "event_id"
+                )}`}
+              >
+                イベントに参加する
+              </a>
+            </div>
           ) : (
             <p className={"text-xl"}>10月12日リリース予定</p>
           )}
