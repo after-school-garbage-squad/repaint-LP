525dafaac6d2361c972163bb3b95d776fccdda30
Bayathy <misosoup0321@icloud.com>
2023-10-11 23:25:13 +0900

    feat(WIP): マニュアルの作成

--- src/ui/ManualSection/manual-section.tsx
+++ src/ui/ManualSection/manual-section.tsx
@@ -1,15 +1,18 @@
 import { SectionLayout } from "../shared/SectionLayout";
 
 import { ManualCard } from "./components/manual-card";
-import { manualText } from "./manual";
 
 export const ManualSection: React.FC = () => {
   return (
     <SectionLayout title="マニュアル" subTitle="manual" sectionId="#manual">
-      <div className="mt-4 flex flex-col gap-4">
-        {manualText.map((manual) => (
-          <ManualCard key={manual.title} {...manual} />
-        ))}
+      <div className="mt-4 flex flex-col items-center">
+        <ManualCard title="title">
+          <div>
+            <p>
+              このアプリは、イベント会場を周りながら「パレット」を集めて、写真の完成を目指すアプリです。
+            </p>
+          </div>
+        </ManualCard>
       </div>
     </SectionLayout>
   );
