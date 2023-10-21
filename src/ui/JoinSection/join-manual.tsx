f5e002d612899af1b16e9aafe29bd51598b66bbd
Bayathy <misosoup0321@icloud.com>
2023-10-21 10:07:57 +0900

    feat: 参加ボタンの復活

---
+++ src/ui/JoinSection/join-manual.tsx
@@ -0,0 +1,27 @@
+export const joinManualText = [
+  {
+    title: "Testflightのインストール",
+    children: (
+      <p>
+        <a href={"https://apps.apple.com/jp/app/testflight/id899247664"}>
+          こちら
+        </a>
+        からTestflightアプリをダウンロードします。
+      </p>
+    ),
+  },
+  {
+    title: "Re:paintのインストール",
+    children: (
+      <p>
+        Testflightアプリをダウンロード後、
+        <a href="https://testflight.apple.com/join/ZreoO6ws">こちら</a>
+        からRe:paintをインストールしてください。
+      </p>
+    ),
+  },
+  {
+    title: "イベントへの参加",
+    text: "アプリのインストール後、パンフレットやポスターの指示に従ってイベントに参加してください。",
+  },
+];
